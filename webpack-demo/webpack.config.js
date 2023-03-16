e1ee5db0d3570c3cd8b4049a5200600e36dd11b8
nehamaheshwari-adobe <124973418+nehamaheshwari-adobe@users.noreply.github.com>
2023-03-16 22:28:02 +0530

    Add files via upload

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./resources/script/script.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
