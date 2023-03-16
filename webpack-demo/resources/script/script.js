32de1977024330f0f9a30687499a940b9212b7c2
nehamaheshwari-adobe <124973418+nehamaheshwari-adobe@users.noreply.github.com>
2023-03-16 22:31:22 +0530

    Add files via upload

---
+++ webpack-demo/resources/script/script.js
@@ -0,0 +1,8 @@
+//import {user, UserName } from './vendor.js';
+//import COMMON from "./vendor.js";
+import "../style/main.sass";
+
+//console.log(user.age);
+//console.log(UserName);
+
+//console.log(COMMON.cancel);
---
+++ webpack-demo/resources/script/vendor.js
@@ -0,0 +1,21 @@
+const UserName = "ABC";
+
+const user = {
+    age:22
+}
+
+const COMMON = {
+    cancel:"cancel",
+    close:"close"
+}
+
+const ABOUTUS = {
+    title:"title1"
+}
+
+const CONTACTUS = {
+    title:"title2"
+}
+
+//export {user, UserName};
+export default Object.seal(COMMON);
